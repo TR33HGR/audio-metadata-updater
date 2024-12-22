a606a9e76c1f0cb86178ce531ab93e1607bea0be
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2024-12-22 11:04:07 +0000

    Add function to filter lastfm tags

--- src/audio_metadata_updater/last_fm_metadata_finder.py
+++ src/audio_metadata_updater/last_fm_metadata_finder.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from difflib import SequenceMatcher
 import os
 from typing import List
 import requests
@@ -11,6 +12,14 @@ class LastFMMetadata:
     album: str
     track_name: str
     tags: List[str]
+
+
+def are_duplicates(tag1: str, tag2: str, threshold=0.7) -> bool:
+    return SequenceMatcher(None, tag1, tag2).ratio() > threshold
+
+
+def longest_tag(tag1, tag2):
+    return tag1 if len(tag1) >= len(tag2) else tag2
 
 
 class LastFMMetadataFinder():
@@ -45,3 +54,18 @@ class LastFMMetadataFinder():
             metadata.get("name"),
             [tag["name"] for tag in metadata.get("toptags").get("tag")]
         )
+
+    def filter_tags(self, tags: List[str]) -> List[str]:
+        filtered_tags = []
+        tags = [tag.lower() for tag in tags]
+
+        for tag in tags:
+            for i, filtered_tag in enumerate(filtered_tags):
+                if are_duplicates(tag, filtered_tag):
+                    filtered_tags[i] = longest_tag(tag, filtered_tag)
+                    break
+            else:
+                filtered_tags.append(tag)
+
+        filtered_tags = [tag.title() for tag in filtered_tags]
+        return filtered_tags
--- tests/test_last_fm_metadata_finder.py
+++ tests/test_last_fm_metadata_finder.py
@@ -34,3 +34,54 @@ def test_last_fm_metadata_finder_finds_metadata_given_extracted_metadata():
         found_metadata.track_name,
         equal_to_ignoring_case("RUN2U")
     )
+
+
+def test_last_fm_metadata_finder_tags_filter_removes_duplicate_tags():
+    expected_tags = ["K-Pop"]
+
+    # Given
+    metadata_finder = LastFMMetadataFinder()
+    found_tags = ["K-Pop", "Kpop"]
+
+    # When
+    filtered_tags = metadata_finder.filter_tags(found_tags)
+
+    # Then
+    assert_that(
+        filtered_tags,
+        contains_inanyorder(*expected_tags)
+    )
+
+
+def test_last_fm_metadata_finder_tags_filter_favours_more_specific_tags():
+    expected_tags = ["K-Pop", "Electroclash"]
+
+    # Given
+    metadata_finder = LastFMMetadataFinder()
+    found_tags = ["Pop", "K-Pop", "Electronic", "Electroclash"]
+
+    # When
+    filtered_tags = metadata_finder.filter_tags(found_tags)
+
+    # Then
+    assert_that(
+        filtered_tags,
+        contains_inanyorder(*expected_tags)
+    )
+
+
+def test_last_fm_metadata_finder_tags_filter_formats_tags_to_title_case():
+    expected_tags = ["K-Pop"]
+
+    # Given
+    metadata_finder = LastFMMetadataFinder()
+    found_tags = ["k-pop"]
+
+    # When
+    filtered_tags = metadata_finder.filter_tags(found_tags)
+
+    # Then
+    assert_that(
+        filtered_tags,
+        contains_inanyorder(*expected_tags)
+    )
